d9035c75ec7c6e60eb587a20c524b467e9f60cf9
marleigh <volcana@gmail.com>
2021-11-22 17:37:38 -0800

    styling css

--- src/components/Module.js
+++ src/components/Module.js
@@ -2,11 +2,16 @@
 export default function Module(props) {
     console.log(props)
     return (
-        <section>
-            <div><span className="module--location">{props.item.location}</span> <a href="">View on Google Maps</a></div>
-            <h2>{props.item.title}</h2>
-            <p>{props.item.startDate} - {props.item.endDate}</p>
-            <p>{props.item.description}</p>
+        <section className="section--module">
+            <div>
+                <img src={props.item.imageUrl} className="module--img"></img>
+            </div>
+            <div>
+                <span className="module--location">{props.item.location}</span> <a href="#">View on Google Maps</a>
+                <h2>{props.item.title}</h2>
+                <p className="module--date">{props.item.startDate} - {props.item.endDate}</p>
+                <p className="module--desc">{props.item.description}</p>
+            </div>
         </section>
     )
 }
